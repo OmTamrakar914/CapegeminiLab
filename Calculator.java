b937b3cd14a71f5e5a7b6b34796137b927e70a4a
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-07 13:09:47 +0530

    2 commit

--- Calculator.java
+++ Calculator.java
@@ -24,7 +24,23 @@ public class Calculator{
 
         int opt = sc.nextInt();
 
-
+        if(opt == 1){
+            Addition add = new Addition();
+            System.out.println("Answer = "+add.add(a,b));
+        }
+        if(opt == 2){
+            Substraction sub = new Substraction();
+            System.out.println("Answer = "+sub.Substract(a,b));
+        }if(opt == 3){
+            Multiplication mult = new Multiplication();
+            System.out.println("Answer = "+mult.multiply(a,b));
+        }if(opt == 4){
+            division obj =new division();
+            System.out.println("Answer = "+obj.divide(a,b));
+        }else{
+            System.out.println("Invalid output");
+        }
+        
        
 
 
