1a8a8a3f8a903ccfef5aebb01cf613cce3814e45
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-07 12:54:58 +0530

    first commit

--- Calculator.java
+++ Calculator.java
@@ -9,5 +9,24 @@ public class Calculator{
         System.out.println("--------Calculator--------");
 
 
+
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+
+        System.out.println("-----select operation---------");
+        System.out.println(" 1. Addition");
+        System.out.println(" 2. Substraction ");
+        System.out.println(" 3. Multiplication");
+        System.out.println(" 4. Division");
+
+        int opt = sc.nextInt();
+
+
+       
+
+
      }
 }
